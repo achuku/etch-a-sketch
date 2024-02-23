85b7cfc2693dff762dc149fb07a7f9b623e8741e
Anyamba <achuku@gmail.com>
2024-02-23 15:43:41 +0300

    Standardized class names for grid items

--- eas.js
+++ eas.js
@@ -3,65 +3,65 @@ container.className = 'container';
 body.appendChild(container);
 
 const div1 = document.createElement('div');
-div1.className = 'grid-square-1';
+div1.className = 'grid-item';
 container.appendChild(div1);
 
 const div2 = document.createElement('div');
-div1.className = 'grid-square-2';
+div1.className = 'grid-item';
 container.appendChild(div2);
 
 const div3 = document.createElement('div');
-div1.className = 'grid-square-3';
+div1.className = 'grid-item';
 container.appendChild(div3);
 
 const div4 = document.createElement('div');
-div1.className = 'grid-square-4';
+div1.className = 'grid-item';
 container.appendChild(div4);
 
 const div5 = document.createElement('div');
-div1.className = 'grid-square-5';
+div1.className = 'grid-item';
 container.appendChild(div5);
 
 const div6 = document.createElement('div');
-div1.className = 'grid-square-6';
+div1.className = 'grid-item';
 container.appendChild(div6);
 
 const div7 = document.createElement('div');
-div1.className = 'grid-square-7';
+div1.className = 'grid-item';
 container.appendChild(div7);
 
 const div8 = document.createElement('div');
-div1.className = 'grid-square-8';
+div1.className = 'grid-item';
 container.appendChild(div8);
 
 const div9 = document.createElement('div');
-div1.className = 'grid-square-9';
+div1.className = 'grid-item';
 container.appendChild(div9);
 
 const div10 = document.createElement('div');
-div1.className = 'grid-square-10';
+div1.className = 'grid-item';
 container.appendChild(div10);
 
 const div11 = document.createElement('div');
-div1.className = 'grid-square-11';
+div1.className = 'grid-item';
 container.appendChild(div11);
 
 const div12 = document.createElement('div');
-div1.className = 'grid-square-12';
+div1.className = 'grid-item';
 container.appendChild(div12);
 
 const div13 = document.createElement('div');
-div1.className = 'grid-square-13';
+div1.className = 'grid-item';
 container.appendChild(div13);
 
 const div14 = document.createElement('div');
-div1.className = 'grid-square-14';
+div1.className = 'grid-item';
 container.appendChild(div14);
 
 const div15 = document.createElement('div');
-div1.className = 'grid-square-15';
+div1.className = 'grid-item';
 container.appendChild(div15);
 
 const div16 = document.createElement('div');
-div1.className = 'grid-square-16';
+div1.className = 'grid-item';
 container.appendChild(div16);
