173946ad0fd1eb5e720d1c2c7aba3e8d8179ac21
Anyamba <achuku@gmail.com>
2024-02-23 15:37:47 +0300

    Created 16 divs

---
+++ eas.js
@@ -0,0 +1,67 @@
+const container = document.createElement('div');
+container.className = 'container';
+body.appendChild(container);
+
+const div1 = document.createElement('div');
+div1.className = 'grid-square-1';
+container.appendChild(div1);
+
+const div2 = document.createElement('div');
+div1.className = 'grid-square-2';
+container.appendChild(div2);
+
+const div3 = document.createElement('div');
+div1.className = 'grid-square-3';
+container.appendChild(div3);
+
+const div4 = document.createElement('div');
+div1.className = 'grid-square-4';
+container.appendChild(div4);
+
+const div5 = document.createElement('div');
+div1.className = 'grid-square-5';
+container.appendChild(div5);
+
+const div6 = document.createElement('div');
+div1.className = 'grid-square-6';
+container.appendChild(div6);
+
+const div7 = document.createElement('div');
+div1.className = 'grid-square-7';
+container.appendChild(div7);
+
+const div8 = document.createElement('div');
+div1.className = 'grid-square-8';
+container.appendChild(div8);
+
+const div9 = document.createElement('div');
+div1.className = 'grid-square-9';
+container.appendChild(div9);
+
+const div10 = document.createElement('div');
+div1.className = 'grid-square-10';
+container.appendChild(div10);
+
+const div11 = document.createElement('div');
+div1.className = 'grid-square-11';
+container.appendChild(div11);
+
+const div12 = document.createElement('div');
+div1.className = 'grid-square-12';
+container.appendChild(div12);
+
+const div13 = document.createElement('div');
+div1.className = 'grid-square-13';
+container.appendChild(div13);
+
+const div14 = document.createElement('div');
+div1.className = 'grid-square-14';
+container.appendChild(div14);
+
+const div15 = document.createElement('div');
+div1.className = 'grid-square-15';
+container.appendChild(div15);
+
+const div16 = document.createElement('div');
+div1.className = 'grid-square-16';
+container.appendChild(div16);
