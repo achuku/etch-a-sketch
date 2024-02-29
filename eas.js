cc7f213336aae431ba7adda88714b2e9112b983b
Anyamba <achuku@gmail.com>
2024-02-29 17:23:17 +0300

    Created all 16 divs, giving different background colors for easy visibility

--- eas.js
+++ eas.js
@@ -29,48 +29,59 @@ div5.className = 'grid-item';
 container.appendChild(div5);
 div5.style.backgroundColor = 'black';
 
-/*
+
 const div6 = document.createElement('div');
-div1.className = 'grid-item';
+div6.className = 'grid-item';
 container.appendChild(div6);
+div6.style.backgroundColor = 'white';
 
 const div7 = document.createElement('div');
-div1.className = 'grid-item';
+div7.className = 'grid-item';
 container.appendChild(div7);
+div7.style.backgroundColor = 'red';
 
 const div8 = document.createElement('div');
-div1.className = 'grid-item';
+div8.className = 'grid-item';
 container.appendChild(div8);
+div8.style.backgroundColor = 'purple';
 
 const div9 = document.createElement('div');
-div1.className = 'grid-item';
+div9.className = 'grid-item';
 container.appendChild(div9);
+div9.style.backgroundColor = 'beige';
 
 const div10 = document.createElement('div');
-div1.className = 'grid-item';
+div10.className = 'grid-item';
 container.appendChild(div10);
+div10.style.backgroundColor = 'pink';
 
 const div11 = document.createElement('div');
-div1.className = 'grid-item';
+div11.className = 'grid-item';
 container.appendChild(div11);
+div11.style.backgroundColor = 'turquoise';
 
 const div12 = document.createElement('div');
-div1.className = 'grid-item';
+div12.className = 'grid-item';
 container.appendChild(div12);
+div12.style.backgroundColor = 'brown';
 
 const div13 = document.createElement('div');
-div1.className = 'grid-item';
+div13.className = 'grid-item';
 container.appendChild(div13);
+div13.style.backgroundColor = 'silver';
 
 const div14 = document.createElement('div');
-div1.className = 'grid-item';
+div14.className = 'grid-item';
 container.appendChild(div14);
+div14.style.backgroundColor = 'olive';
 
 const div15 = document.createElement('div');
-div1.className = 'grid-item';
+div15.className = 'grid-item';
 container.appendChild(div15);
+div15.style.backgroundColor = 'moccasin';
 
 const div16 = document.createElement('div');
-div1.className = 'grid-item';
+div16.className = 'grid-item';
 container.appendChild(div16);
- */
+div16.style.backgroundColor = 'navy';
+ 
