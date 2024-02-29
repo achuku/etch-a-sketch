85ef53d2f6b9b3d69cb1c9ea99bdb6de73f71e94
Anyamba <achuku@gmail.com>
2024-02-29 16:38:14 +0300

    Created 3 more divs

--- eas.js
+++ eas.js
@@ -2,25 +2,28 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.className = 'container';
 body.appendChild(container);
-container.style.color = 'red';
+container.style.color = 'gray';
 
 const div1 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div1);
 div1.style.color = 'blue';
-/* 
+
 const div2 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div2);
+div2.style.color = 'yellow';
 
 const div3 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div3);
-
+div3.style.color = 'orange';
+ 
 const div4 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div4);
-
+div4.style.color = 'green';
+/*
 const div5 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div5);
