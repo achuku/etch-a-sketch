2de39f013e171f8273164f795da356f633d1d48d
Anyamba <achuku@gmail.com>
2024-02-29 16:51:26 +0300

    Amended style option for grid all divs from color to backgroundColor

--- eas.js
+++ eas.js
@@ -2,32 +2,34 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.className = 'container';
 body.appendChild(container);
-container.style.color = 'gray';
+container.style.backgroundColor = 'gray';
 
 const div1 = document.createElement('div');
-div1.className = 'grid-item';
+div1.className = 'grid-item first-item';
 container.appendChild(div1);
-div1.style.color = 'blue';
+div1.style.backgroundColor = 'blue';
 
 const div2 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div2);
-div2.style.color = 'yellow';
+div2.style.backgroundColor = 'yellow';
 
 const div3 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div3);
-div3.style.color = 'orange';
+div3.style.backgroundColor = 'green';
  
 const div4 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div4);
-div4.style.color = 'green';
-/*
-const div5 = document.createElement('div');
-div1.className = 'grid-item';
-container.appendChild(div5);
+div4.style.backgroundColor = 'orange';
 
+const div5 = document.createElement('div');
+div1.className = 'grid-item last-item';
+container.appendChild(div5);
+div5.style.color = 'black';
+
+/*
 const div6 = document.createElement('div');
 div1.className = 'grid-item';
 container.appendChild(div6);
