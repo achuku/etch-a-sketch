ddcb16fed671aede464d955f75143d677a809d5a
Anyamba <achuku@gmail.com>
2024-03-06 12:47:41 +0300

    Added button to top of page

--- eas.js
+++ eas.js
@@ -4,18 +4,22 @@ const container = document.createElement('div');
 body.appendChild(container).className = 'container';
 body.style.backgroundColor = 'gray';
 
+const btn = document.createElement('button');
+container.appendChild(btn).className = 'button';
+btn.textContent = 'Change Number Of Squares';
 
-    function createGridItems(rows, columns){
-        //container.style.setProperty ('grid-rows', rows);
-        //container.style.setProperty ('grid-columns', columns);
-        for (i = 0; i < (rows * columns); i++) {
-            let item = document.createElement('div');
-            container.appendChild(item).className = 'grid-item';
-            item.addEventListener ('mouseover',function bgChange(){
-                
-                item.style.backgroundColor = 'white'; 
-            });
-        };
+
+function createGridItems(rows, columns){
+    //container.style.setProperty ('grid-rows', rows);
+    //container.style.setProperty ('grid-columns', columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let item = document.createElement('div');
+        container.appendChild(item).className = 'grid-item';
+        item.addEventListener ('mouseover',function bgChange(){
+            
+            item.style.backgroundColor = 'white'; 
+        });
     };
-    createGridItems(16,16);
+};
+createGridItems(16,16);
  
